2c6ebb1742343b84faa6614ba859a537a900a963
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-05-11 10:59:51 +0200

    Implement sensitivity sampling.

---
+++ yace/clustering/kmeans.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+import scipy.sparse as sp
+
+from numba import jit
+from sklearn.metrics.pairwise import _euclidean_distances
+from sklearn.utils.extmath import stable_cumsum
+from sklearn.utils.validation import check_random_state
+from sklearn.utils.extmath import row_norms
+
+
+def kmeans_plusplus(X, n_clusters, x_squared_norms=None, random_state=None, n_local_trials=None):
+    """Computational component for initialization of n_clusters by
+    k-means++. Prior validation of data is assumed.
+
+    Parameters
+    ----------
+    X : {ndarray, sparse matrix} of shape (n_samples, n_features)
+        The data to pick seeds for.
+
+    n_clusters : int
+        The number of seeds to choose.
+
+    x_squared_norms : ndarray of shape (n_samples,)
+        Squared Euclidean norm of each data point.
+
+    random_state : RandomState instance
+        The generator used to initialize the centers.
+        See :term:`Glossary <random_state>`.
+
+    n_local_trials : int, default=None
+        The number of seeding trials for each center (except the first),
+        of which the one reducing inertia the most is greedily chosen.
+        Set to None to make the number of trials depend logarithmically
+        on the number of seeds (2+log(k)); this is the default.
+
+    Returns
+    -------
+    center_indices : ndarray of shape (n_clusters,)
+        The index location of the chosen centers in the data array X. For a
+        given index and center, X[index] = center.
+    """
+    n_samples = X.shape[0]
+
+    random_state = check_random_state(random_state)
+
+    if x_squared_norms is None:
+        x_squared_norms = row_norms(X, squared=True)
+
+    # Set the number of local seeding trials if none is given
+    if n_local_trials is None:
+        # This is what Arthur/Vassilvitskii tried, but did not report
+        # specific results for other than mentioning in the conclusion
+        # that it helped.
+        n_local_trials = 2 + int(np.log(n_clusters))
+
+    # Pick first center randomly and track index of point
+    center_id = random_state.randint(n_samples)
+    center_indices = np.full(n_clusters, -1, dtype=int)
+    center_indices[0] = center_id
+
+    # Initialize list of closest distances and calculate current potential
+    closest_dist_sq = _euclidean_distances(
+        X[[center_id]], X, Y_norm_squared=x_squared_norms, squared=True
+    )
+    current_pot = closest_dist_sq.sum()
+
+    # Pick the remaining n_clusters-1 points
+    for c in range(1, n_clusters):
+        # Choose center candidates by sampling with probability proportional
+        # to the squared distance to the closest existing center
+        rand_vals = random_state.random_sample(n_local_trials) * current_pot
+        candidate_ids = np.searchsorted(stable_cumsum(closest_dist_sq), rand_vals)
+        # XXX: numerical imprecision can result in a candidate_id out of range
+        np.clip(candidate_ids, None, closest_dist_sq.size - 1, out=candidate_ids)
+
+        # Compute distances to center candidates
+        distance_to_candidates = _euclidean_distances(
+            X[candidate_ids], X, Y_norm_squared=x_squared_norms, squared=True
+        )
+        
+        # update closest distances squared and potential for each candidate
+        np.minimum(closest_dist_sq, distance_to_candidates, out=distance_to_candidates)
+        candidates_pot = distance_to_candidates.sum(axis=1)
+
+        # Decide which candidate is the best
+        best_candidate = np.argmin(candidates_pot)
+        current_pot = candidates_pot[best_candidate]
+        closest_dist_sq = distance_to_candidates[best_candidate]
+        best_candidate = candidate_ids[best_candidate]
+
+        # Permanently add best center candidate found in local tries
+        center_indices[c] = best_candidate
+
+    return center_indices
---
+++ yace/coresets/sensitivity_sampling.py
@@ -0,0 +1,132 @@
+from pathlib import Path
+
+import numpy as np
+import scipy.sparse as sp_sparse
+
+from numba import jit
+from scipy.sparse import issparse, csr_array, csr_matrix
+from sklearn.cluster import KMeans
+from sklearn.metrics import consensus_score
+from sklearn.utils.extmath import row_norms
+from sklearn.utils.validation import check_random_state
+from sklearn.metrics.pairwise import _euclidean_distances
+
+from yace.clustering.kmeans import kmeans_plusplus
+
+
+class SensitivitySampling:
+    def __init__(self, n_clusters: int, coreset_size: int, output_dir: Path) -> None:
+        self._n_clusters = n_clusters
+        self._coreset_size = coreset_size
+        self._output_dir = output_dir
+    
+    def run(self, X):
+        D = self._compute_kmeans_cluster_distances(X=X)
+
+        cluster_labels = np.argmin(D, axis=1)
+        point_costs = np.min(D, axis=1)
+
+        sampled_indices, sampled_weights = self._sample_points_for_coreset(point_costs=point_costs)
+
+        center_weights = self._compute_cluster_center_weights(
+            coreset_indices=sampled_indices,
+            coreset_weights=sampled_weights,
+            cluster_labels=cluster_labels,
+        )
+
+        # First add sampled points to the coreset
+        coreset_points = [X[sampled_indices]]
+
+        # Then add k-means++ cluster centers to the coreset
+        for c in range(self._n_clusters):
+            cluster_members = X[cluster_labels == c]
+
+            # Compute the mean of the clusters
+            centroid = cluster_members.mean(axis = 0)
+
+            if issparse(X):
+                centroid = sp_sparse.csr_array(centroid)
+
+            coreset_points.append(centroid)
+
+        # Combine everything
+        if issparse(X):
+            coreset_points = sp_sparse.vstack(coreset_points, format="csr")
+        else:
+            coreset_points = np.vstack(coreset_points)
+
+        # Include the weights
+        coreset_weights = np.concatenate([sampled_weights, center_weights])
+        if issparse(X):
+            weighted_coreset_points = sp_sparse.hstack([coreset_weights[:,None], coreset_points])
+        else:
+            weighted_coreset_points = np.hstack([coreset_weights[:,None], coreset_points])
+
+        output_file_path = self._output_dir / "results.npz"
+        np.savez_compressed(output_file_path, matrix=weighted_coreset_points)
+
+    def _compute_kmeans_cluster_distances(self, X):
+        # Precompute squared norms of data points
+        x_squared_norms = row_norms(X, squared=True)
+        random_state = check_random_state(None)
+        center_indices = kmeans_plusplus(
+            X=X,
+            n_clusters=self._n_clusters,
+            random_state=random_state,
+            x_squared_norms=x_squared_norms,
+        )
+
+        D = _euclidean_distances(
+            X=X,
+            Y=X[center_indices],
+            X_norm_squared=x_squared_norms,
+            Y_norm_squared=x_squared_norms[center_indices],
+            squared=True
+        )
+
+        return D 
+
+    def _sample_points_for_coreset(self, point_costs: np.ndarray):
+        n_points = point_costs.shape[0]
+
+        # Compute cost(X)
+        total_cost = np.sum(point_costs)
+
+        # print(f"Total cost: {total_cost}")
+
+        # Compute the sampling distribution: cost(p, X)/cost(X)
+        sampling_distribution = point_costs / total_cost
+
+        sampled_indices = np.random.choice(a=n_points, size=self._coreset_size, replace=True, p=sampling_distribution)
+
+        # We scale the cost of the sampled point by a factor of T i.e. T * cost(p, X)
+        scaled_costs = self._coreset_size * point_costs[sampled_indices]
+
+        # The weight of the sampled point is now: cost(A) / (T*cost(p,X))
+        weights = total_cost / scaled_costs
+
+        # for i in range(len(sampled_indices)):
+        #     print(f"Sampled point {sampled_indices[i]:3}  cost={scaled_costs[i]:0.5}  gets weight={weights[i]:0.5}")
+
+        return sampled_indices, weights
+
+    def _compute_cluster_center_weights(self, coreset_indices: np.ndarray, coreset_weights: np.ndarray, cluster_labels: np.ndarray):
+        # Find the cluster labels of coreset points
+        coreset_cluster_labels = cluster_labels[coreset_indices]
+
+        # The cluster center weight is now sum_{p in coreset and p in C_i} coreset_weight where coreset_weight=cost(A)/(T*cost(p,A))
+        center_weights = np.bincount(coreset_cluster_labels, weights=coreset_weights)
+
+        if center_weights.shape[0] < self._n_clusters:
+            # Ensure that there is weight for each cluster center even if no
+            # points have been assigned to a cluster.
+            m = self._n_clusters - center_weights.shape[0]
+            center_weights = np.pad(center_weights, pad_width=(0, m))
+        
+        # Compute |C_i|  i.e. number of member points in each cluster
+        cluster_indices, member_counts = np.unique(cluster_labels, return_counts=True)
+
+        # Compute max(0, |C_i| - w_i)
+        center_weights = np.maximum(0, member_counts - center_weights)
+
+        return center_weights
