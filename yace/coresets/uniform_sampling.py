bd845377ab9bd92ea0502ab8c1f1be1a5279f7c9
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-05-14 08:45:03 +0200

    Add comments.

--- yace/coresets/uniform_sampling.py
+++ yace/coresets/uniform_sampling.py
@@ -9,9 +9,10 @@ class UniformSampling(SamplingBasedAlgorithm):
     
     def run(self, X):
         n_points = X.shape[0]
+        # Sample T input points uniformly at random without replacement
         sampled_indices = np.random.choice(a=n_points, size=self._coreset_size, replace=False)
-
+        # Add sampled points to the coreset
         coreset_points = X[sampled_indices]
-        coreset_weights = np.ones(shape=self._coreset_size) * (n_points / self._coreset_size)
-
+        # Weigh each coreset point by n / T
+        coreset_weights = (n_points / self._coreset_size) * np.ones(shape=self._coreset_size)
         return coreset_points, coreset_weights
