4963f1a7c3da334e8ddf36884a4f3450700614e1
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-05-20 07:32:47 +0200

    Remove unused method.

--- yace/coresets/sensitivity_sampling.py
+++ yace/coresets/sensitivity_sampling.py
@@ -63,50 +63,6 @@ class SensitivitySampling(SamplingBasedAlgorithm):
 
         return sampled_indices, weights
 
-    def _non_uniform_sample_points_for_coreset_complicated(self, sq_pairwise_distances: np.ndarray):
-
-        # For each input point p in A compute:
-        #  cost(p, K) = min_{k in K} ||p-k||^2
-        # where K is the initial solution
-        point_costs = np.min(sq_pairwise_distances, axis=1)
-
-        n_points = point_costs.shape[0]
-
-        # Scale the cost for each point p to:
-        #  cost(p, K)/cost_{K_p} + 1/|K_p|
-        # where
-        #  - K_p is the set of points belonging to same cluster as p
-        #  - cost_{K_p} = sum_{q in K_p} cost(q, K)
-        #  - K is the initial solution obtained by running k-means++.
-        new_point_costs = point_costs.copy()
-        cluster_labels = np.argmin(sq_pairwise_distances, axis=1)
-        for cluster_index in np.unique(cluster_labels):
-            cluster_member_idx = np.where(cluster_labels == cluster_index)[0]
-            cluster_cost = np.sum(point_costs[cluster_member_idx])
-            cluster_size = cluster_member_idx.shape[0]
-            if cluster_cost > 0:
-                new_point_costs[cluster_member_idx] = point_costs[cluster_member_idx] / cluster_cost
-            new_point_costs[cluster_member_idx] += 1 / cluster_size
-        point_costs = new_point_costs
-
-        # Compute the total cost: cost_A(K) = sum_{p in A} cost_A(p, K)
-        # where K is the initial solution obtained by running k-means++.
-        total_cost = np.sum(point_costs)
-
-        # Compute the sampling distribution: cost_A(p, K) / cost_A(K)
-        sampling_distribution = point_costs / total_cost
-
-        # Sample T points using the computed sampling distribution
-        sampled_indices = np.random.choice(a=n_points, size=self._coreset_size, replace=True, p=sampling_distribution)
-
-        # Compute the weights as 1 / samping_probability * scaling_factor
-        # where scaling_factor is 1 / coreset_size
-        # weights = 1 / (sampling_distribution[sampled_indices])
-        # weights = (weights/(self._coreset_size))
-        weights = total_cost / (self._coreset_size * point_costs[sampled_indices])
-
-        return sampled_indices, weights
-
     def _non_uniform_sample_points_for_coreset_biased(self, sq_pairwise_distances: np.ndarray):
         # For each input point p in A compute:
         #  cost(p, K) = min_{k in K} ||p-k||^2
